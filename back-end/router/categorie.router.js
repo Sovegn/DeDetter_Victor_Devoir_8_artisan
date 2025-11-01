7a6c81cf8591a26574b8010e95ab9cfce0791b41
Sovgn <victor.dedetter59@gmail.com>
2025-11-02 00:26:11 +0100

    fix: correction de bugs

--- back-end/router/categorie.router.js
+++ back-end/router/categorie.router.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const CONTROLLER = require('../controllers/Artisan.controller');
-const { validateId, validateSearch } = require('../middlewares/validation');
+const CONTROLLER = require('../controllers/Categorie.controller');
+const { validateId } = require('../middlewares/validation');
 
-
-router.get('/all', CONTROLLER.getAllArtisans);
-router.get('/get/:id', validateId, CONTROLLER.getArtisan);
-router.get('/categorie/:categorieId', validateId, CONTROLLER.getArtisansByCategorie);
-router.get('/top', CONTROLLER.getArtisansDuMois);
-router.get('/search', validateSearch, CONTROLLER.searchArtisans);
+router.get('/all', CONTROLLER.getAllCategories);
+router.get('/get/:id', validateId, CONTROLLER.getCategorie);
 
 module.exports = router;
